d766f2461b247d9735632cd8a87bf27a1ece74b6
tisha-akter <tishaakter.jui79@gmail.com>
2023-02-06 15:44:48 +0600

    solved 3rd solution

---
+++ graterThan5.js
@@ -0,0 +1,32 @@
+/* emon ekta function banao jeta ekti array input hisabe nibe.sei array er moddhe 5 ar cheye boro sokkha count korbe. jar input-
+input : [-1,2,-3,4,5,6,-7,-8,-9,10]
+output:3 
+*/ 
+
+/* concept
+let num = 4;
+if (num < 5 ){
+    console.log('yes')
+}
+else {
+    console.log('no')
+}
+*/ 
+
+
+// solution
+let numbers = [-1,2,-3,4,5,6,-7,8,-9,10];
+let count = 0;
+
+for(let i = 0; i<numbers.length; i++){
+    const element = numbers[i];
+    // console.log(element)
+    if(element > 5){
+        // console.log("Yes");
+        count = count +1;
+    }
+    else{
+        // console.log("No");
+    }
+}
+console.log(count);
--- summation.js
+++ summation.js
@@ -9,15 +9,15 @@
 // sum = num1+num2; 
 // console.log(sum);
 
-//* solution*//
-// let n = 10;
-// let sum = 0;
-// for(let i = 1; i <= n; i++){
-//     sum = sum + i;
-// }
-// console.log(sum);
+* solution*//
+let n = 10;
+let sum = 0;
+for(let i = 1; i <= n; i++){
+    sum = sum + i;
+}
+console.log(sum);
 
 
-// //* or we can use just a law ' n(n+1)/2  ' *//
-// let sum2 = (n*(n+1))/2;
-// console.log('sum2 ', sum2)
+//* or we can use just a law ' n(n+1)/2  ' *//
+let sum2 = (n*(n+1))/2;
+console.log('sum2 ', sum2)
