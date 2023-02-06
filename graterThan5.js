591978cdb9721d1b05f29c3463d4b43fdb767c73
tisha-akter <tishaakter.jui79@gmail.com>
2023-02-06 15:55:25 +0600

    solved graterThan5

--- graterThan5.js
+++ graterThan5.js
@@ -14,19 +14,38 @@ else {
 */ 
 
 
-// solution
-let numbers = [-1,2,-3,4,5,6,-7,8,-9,10];
-let count = 0;
+// let numbers = [-1,2,-3,4,5,6,-7,8,-9,10];
+// let count = 0;
 
-for(let i = 0; i<numbers.length; i++){
-    const element = numbers[i];
-    // console.log(element)
-    if(element > 5){
-        // console.log("Yes");
-        count = count +1;
+// for(let i = 0; i<numbers.length; i++){
+//     const element = numbers[i];
+//     // console.log(element)
+//     if(element > 5){
+//         // console.log("Yes");
+//         count = count +1;
+//     }
+//     else{
+//         // console.log("No");
+//     }
+// }
+// console.log(count);
+
+
+// solution
+function graterThanFive(numbers){
+    let count = 0;
+
+    for(i = 0; i<numbers.length; i++){
+        const element = numbers[i];
+        if (element>5){
+            count++;
+        }
+        else{
+            continue
+        }
     }
-    else{
-        // console.log("No");
-    }
+    return count;
 }
-console.log(count);
+let result = graterThanFive([-1,2,-3,4,5,6,-7,8,-9,10]);
+console.log(result);
+
